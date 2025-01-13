cc671ce0dfd47f7946685e7eceeffee1e98b4eed
Rosanne Saparamadu <rosanne.thms@gmail.com>
2025-01-13 22:43:48 +1300

    update

--- pages/login.js
+++ pages/login.js
@@ -4,17 +4,22 @@ import { signIn, getProviders } from "next-auth/react";
 
 export default function Login({ providers }) {
   return (
-    <div className="flex justify-center items-center h-screen text-center flex-col space-y-6 bg-[#050505]">
+    <div className="flex justify-center items-center h-screen text-center flex-col space-y-6">
       <h1 className="text-3xl font-semibold text-white">Welcome to the Spotify Clone</h1>
       <p className="text-lg text-gray-300">
         Sign in with your Spotify account to start exploring music, playlists, and more.
       </p>
       <div className="space-y-4 mt-6">
-        <Button
-            className="bg-[#1DB954] text-white w-full p-3 rounded-md"
-          >
-            Sign in with Spotify <Login01Icon className="ml-1" size={20} />
-          </Button>
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <Button
+              key={provider.name}
+              className="bg-[#1DB954] text-white w-full p-3 rounded-md"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name} <Login01Icon className="ml-1" size={20} />
+            </Button>
+          ))}
       </div>
     </div>
   );
